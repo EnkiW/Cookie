d0dbde8d15240118bbbb69573ab5c5dccd9ef54f
EnkiW <ytcahekyttpocto@gmail.com>
2023-03-08 14:59:16 +0200

    Finished

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-document.cookie = "name = Олександр";
-document.cookie = "age = 15";
-const cookies = document.cookie.split(';');
-let name, age;
+let name = prompt("Будь ласка, введіть ваше ім'я:");
+let age = prompt("Будь ласка, введіть ваш вік:");
+document.cookie = "name=" + name + ";";
+document.cookie = "age=" + age + ";";
 
-for(let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].split('=');
-    if(cookie[0].trim() === 'name') {
-        name = cookie[1].trim();
-    } else if(cookie[0].trim() === 'age') {
-        age = cookie[1].trim();
+let cookies = document.cookie.split(";");
+for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) == ' ') {
+        cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf("name=") == 0) {
+        document.write("<p>Ім'я: " + cookie.substring("name=".length, cookie.length) + "</p>");
+    }
+    if (cookie.indexOf("age=") == 0) {
+        document.write("<p>Вік: " + cookie.substring("age=".length, cookie.length) + "</p>");
     }
 }
-
-console.log(`Моє ім'я: ${name}`);
-console.log(`Мій вік: ${age}`);
-
